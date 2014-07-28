a04bfe18cce050c3de650ea6641822f8bd2aedf1
JohnsonLu <johnson4932@gmail.com>
2014-07-28 13:52:17 +0800

    default parser

--- fallzu-bootstrap-select.js
+++ fallzu-bootstrap-select.js
@@ -2,11 +2,37 @@
     "use strict";
 
     $.fn.fallzuSelectpicker = function(devOption) {
+        var root = $(this);
+        var render = function (template) {
+            root.after(
+                '<div class="dropdown">' +
+                    '<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">' +
+                        '<span class="pull-left">未指定</span>' +
+                        '<span class="caret"></span>' +
+                    '</button>' +
+                    '<ul class="dropdown-menu" role="menu">' + template + '</ul>' +
+                '</div>'
+            );
+        };
+
+        // Check select element
         if ($(this).is('select')) {
-            $(this).after($(''));
+            var template = '';
+            $(this).children().each(function() {
+                var rootOption = $(this);
+
+                // Check isGroup
+                if (rootOption.is('optgroup')) {
+                    // Coming soon
+                } else {
+                    template += '<li><a href="#">' + rootOption.text() + '</a></li>';
+                }
+
+            });
+
+            render(template);
         }
 
         return $(this);
     };
-
 })(jQuery);
